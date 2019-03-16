793c2fabe5e5324cad6faf614fb4e4807b6a1a73
Wes Baker <wes@wesbaker.com>
2019-03-16 09:15:03 -0400

    Replace moment with date-fns, async fixes

--- check.js
+++ check.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
 const axios = require("axios");
-const moment = require("moment");
+const format = require("date-fns/format");
+const subDays = require("date-fns/sub_days");
 const getAttachment = require("./lib/Play").getAttachment;
 
 const testRun = process.argv.includes("--test");
@@ -9,8 +10,11 @@ if (testRun) {
   if (typeof axios !== "function") {
     console.error("axios was not loaded.");
   }
-  if (typeof moment !== "function") {
-    console.error("moment was not loaded.");
+  if (typeof format !== "function") {
+    console.error("format was not loaded.");
+  }
+  if (typeof subDays !== "function") {
+    console.error("subDays was not loaded.");
   }
   console.log("Debug: Nemecord would post now.");
   process.exit(0);
@@ -21,28 +25,27 @@ Raven.config(process.env.SENTRY_DSN);
 
 const url = process.env.DISCORD_WEBHOOK_URL;
 
-module.exports = (req, res) => {
-  axios
+module.exports = async (req, res) => {
+  await axios
     .get("https://nemestats.com/api/v2/PlayedGames/", {
       params: {
         gamingGroupId: process.env.GAMING_GROUP_ID,
-        datePlayedFrom: moment()
-          .subtract(1, "days")
-          .format("YYYY-MM-DD"),
-        datePlayedTo: moment()
-          .subtract(1, "days")
-          .format("YYYY-MM-DD")
+        datePlayedFrom: format(subDays(new Date(), 1), "YYYY-MM-DD"),
+        datePlayedTo: format(subDays(new Date(), 1), "YYYY-MM-DD")
       }
     })
     .then(({ data: { playedGames } }) => {
-      playedGames.forEach(playData => {
-        getAttachment(playData).then(attachment => {
-          axios.post(url, attachment).catch(error => {
+      const promises = playedGames.map(playData => {
+        return getAttachment(playData).then(attachment => {
+          return axios.post(url, attachment).catch(error => {
             if (error) Raven.captureException(error);
           });
         });
       });
-      res.end(`${playedGames.length} plays sent to Discord.`);
+
+      return Promise.all(promises).then(() => {
+        res.end(`${playedGames.length} plays sent to Discord.`);
+      });
     })
     .catch(err => {
       Raven.captureException(err);
